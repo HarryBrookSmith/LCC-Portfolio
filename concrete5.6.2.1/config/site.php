2dcdfe0b2ae63cfd5fcbd6a709e61b11a53fd95f
HarryBrookSmith <hello@harrybrook.com>
2014-01-19 14:01:06 +0000

    Initial Setup
    Setup and initial styling

---
+++ concrete5.6.2.1/config/site.php
@@ -0,0 +1,6 @@
+<?php 
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'concrete5local');
+define('DB_PASSWORD', 'hazza326');
+define('DB_DATABASE', 'concrete5local');
+define('PASSWORD_SALT', 'MgYETmMC4GL9KeReSJK4xWr97T2PLomuvirL1k74L62TJu7Tcox9zZhC3EzilaGp');
---
+++ concrete5.6.2.1/themes/LisaCaruccioCamePortfolio2014/default.php
@@ -0,0 +1,18 @@
+<?php  
+defined('C5_EXECUTE') or die("Access Denied.");
+$this->inc('elements/header.php'); ?>
+
+    <div id="central" role="main" class="clearfix">
+		<div id="body">	
+			<?php  
+			$a = new Area('Main');
+			$a->display($c);
+			?>
+		</div> <!-- end of #body -->
+        
+        <aside id="sidebar">
+		
+		</aside>
+    </div>
+
+<?php   $this->inc('elements/footer.php'); ?>
